1ed19f610ebd26e3f5b791c4c5739e17f71b47f5
Geovane Machado <geovane.machadoproject@gmail.com>
2023-03-23 11:15:15 -0300

    Últimas alterações feitas

---
+++ DOM/script.js
@@ -0,0 +1,29 @@
+// getElementById() é a forma de buscar um elemento pelo ID dele:
+//
+//const element = document.getElementById('blog-title')
+//console.log(element)
+
+// getElementByClassName() é a forma de buscar um elemento pela classe dele.
+// const element = document.getElementsByClassName('one');
+// console.log(element)
+
+// getElementByTagName()
+// const element = document.getElementByTagName('meta')
+// console.log(element)
+
+// getElementByTagName()
+// const element = document.querySelector('meta')
+// console.log(element)
+
+//getElementById (element)
+//getElementByClassName (HTMLCollection)
+//getElementByTagName (HTMLCollection)
+//querySelector (element)
+//querySelectorAll (Nodelist)
+
+
+// como manipular conteúdo usando textContent
+
+//const element = document.querySelector('h1')
+//element.textContent += " Olá Devs!"
+//console.log(lement.textContent)
